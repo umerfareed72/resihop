5d0094a24fe61361f3567c69fc3c07bb7ac79e2e
umerfareed72 <umerfareed70@gmail.com>
2022-02-16 01:17:50 +0500

    new

--- src/components/MapViewComponent.js
+++ src/components/MapViewComponent.js
@@ -182,7 +182,7 @@ const MapViewComponent = ({
             <MapViewDirections
               origin={returnOrigin.description}
               destination={returnDestinationMap.description}
-              apikey={'AIzaSyBJIS-db487vhfySNWbG_iicYTyXO_xPTs'}
+              apikey={'AIzaSyBq3-UEY9QO9X45s8w54-mrwjBQekzDlsA'}
               strokeWidth={3}
               strokeColor={'#007BD2'}
               mode="DRIVING"
